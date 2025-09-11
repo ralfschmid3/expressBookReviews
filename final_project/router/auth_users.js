87b3f867f5a9ebcbe241e8e4675541c9c87a320f
ralfschmid3 <ralf.schmid3@gmx.de>
2025-09-11 19:06:34 +0200

    Finished until task 9. Next task 10-13 on promises.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,24 +6,79 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    //write code to check is the username is valid
+    let validusers = users.filter((user) => {
+        (user.username === username);
+    });
+    return validusers;
 }
 
+
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+};    
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+	    data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+	    accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
+
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const isbn = req.params.isbn;
+    const user = req.session.authorization.username;
+    const review = req.query;
+
+    books[isbn].reviews[user] = review;
+    res.status(200).json({message: "Added new review."});
+    //return res.status(300).json({message: "Yet to be implemented"});
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const user = req.session.authorization.username;
+
+    if (books[isbn].reviews[user]) {
+        delete books[isbn].reviews[user];
+        return res.status(200).json({message: "Review deleted."});
+    } else {
+        return res.status(401).json({message: "You did not review this book."});
+    }
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
